99639c1c7e6aa83377b78a06cd2a30dfefc3b07b
Tommy May <46503072+twmay98@users.noreply.github.com>
2021-02-13 18:16:03 -0800

    init commit

--- read.js
+++ read.js
@@ -6,10 +6,10 @@ http.createServer(function (request, response) {
     let q = url.parse(request.url, true);
     const mysql = require("mysql");
     const con = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "webdev"
+        host: "us-cdbr-east-03.cleardb.com",
+        user: "bdd9d9b8d4db5b",
+        password: "2481dad7",
+        database: "heroku_e26bbf5cbd23920"
     });
 
     con.connect(function (err) {
