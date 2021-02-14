00d5a593f60274ac27012732865ec6283a732dd6
Tommy May <46503072+twmay98@users.noreply.github.com>
2021-02-13 17:41:25 -0800

    init commit

---
+++ read.js
@@ -0,0 +1,31 @@
+let http = require('http');
+let url = require("url");
+
+http.createServer(function (request, response) {
+    let names = "";
+    let q = url.parse(request.url, true);
+    const mysql = require("mysql");
+    const con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "webdev"
+    });
+
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("connected");
+        let sql = "SELECT * FROM score";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+            for (i = 0; i < result.length; i++ ) {
+                names = names.concat(result[i].name + ": " + result[i].score + "<br>");
+            }
+            response.writeHead(200, {'Content-type': 'text/html', "Access-Control-Allow-Origin": "*"});
+            response.end(names);
+        });
+    });
+}
+).listen(process.env.PORT || 3000);
+console.log('listening ...');
---
+++ write.js
@@ -0,0 +1,28 @@
+let http = require('http');
+let url = require("url")
+
+http.createServer(function (request, response) {
+    let q = url.parse(request.url, true);
+    const mysql = require("mysql");
+    const con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "webdev"
+    });
+
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("connected");
+        let sql = "INSERT INTO score(name, score) values ('" + q.query["name"] + "', " +  q.query["score"] + ")";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+        });
+    });
+        console.log("server received req");
+        response.writeHead(200, {'Content-type': 'text/html', "Access-Control-Allow-Origin": "*"});
+        response.end("Hello " + q.query["name"] + q.query["score"]);
+    }
+).listen(process.env.PORT || 3000);
+console.log('listening ...');
